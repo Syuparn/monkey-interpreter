1f65c00a9679f0b38ea36c2400318271cdac9ab3
Syuparn <s.hello.spagetti@gmail.com>
2019-08-08 16:37:27 +0900

    add nextToken

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,9 @@
 package lexer
 
+import (
+	"../token"
+)
+
 type Lexer struct {
 	input        string
 	position     int  // 入力における現在読んでいる文字の位置
@@ -8,13 +12,13 @@ type Lexer struct {
 }
 
 func (l *Lexer) readChar() {
-	if l.position >= len(l.input) {
+	if l.readPosition >= len(l.input) {
 		l.ch = 0 // inputの終端に到達したらnull文字セット
 	} else {
 		l.ch = l.input[l.readPosition]
 	}
 	l.position = l.readPosition
-	l.readPosition++
+	l.readPosition += 1
 }
 
 // Lexerのコンストラクタ
@@ -22,4 +26,37 @@ func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	l.readChar() // 一文字目を読みこんでおく
 	return l
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF //newTokenで生成しないのは、null文字をstringで変換できないため？
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
