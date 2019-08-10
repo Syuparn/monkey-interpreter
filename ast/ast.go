0313ce521e66c3de030bdee92ff55c52fc4c5e24
Syuparn <s.hello.spagetti@gmail.com>
2019-08-10 17:00:43 +0900

    introduce let statement

---
+++ ast/ast.go
@@ -0,0 +1,53 @@
+package ast
+
+import (
+	"../token"
+)
+
+type Node interface { // 範疇(category)を扱うインターフェース
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode() // Expressionとの混同をコンパイルエラーにするためのダミーメソッド
+}
+
+type Expression interface {
+	Node
+	expressionNode() // Statementとの混同をコンパイルエラーにするためのダミーメソッド
+}
+
+type Program struct { // プログラム全体(=S)
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // token.Let
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token // token.IDENT
+	Value string
+}
+
+// NOTE: let文は値を返さないが、後で値を返すidentifierも作るのでexpressionにする
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
