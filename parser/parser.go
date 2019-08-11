24d4fbc4d20d0298ad15c8c8cf3de59968e3518f
Syuparn <s.hello.spagetti@gmail.com>
2019-08-11 12:47:27 +0900

    enable to parse let stmt

--- parser/parser.go
+++ parser/parser.go
@@ -29,5 +29,65 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil { // ast.Statementが返ってきた
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken} // Token.Let
+
+	if !p.expectPeek(token.IDENT) { // peekTokenが期待通りなときのみnextToken()
+		return nil
+	}
+
+	//                                 token.IDENT        var name
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: 右辺の式をparseしstmt.Valueにセット（今はセミコロンまで読み飛ばしている）
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+// peekTokenの型がtと等しいかどうか
+// NOTE: 副作用; true(peekTokenが期待通り)のとき内部でnextToken()呼ぶ
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	}
+	return false
 }
