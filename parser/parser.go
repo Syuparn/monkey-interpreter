09aa44473863f47efb6a6ee2b1a05558a326351f
Syuparn <s.hello.spagetti@gmail.com>
2019-08-13 10:58:05 +0900

    implement pratt

--- parser/parser.go
+++ parser/parser.go
@@ -14,6 +14,9 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 // Parser constructor
@@ -121,4 +124,18 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.peekError(t)
 		return false
 	}
+}
+
+// pratt構文解析器の中で各tokenに対し、tokenが前置のとき、中置のときにそれぞれ呼ばれる関数の型
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression // 引数は左側の式
+)
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
 }
