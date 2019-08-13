9693407eafb8b93f6791e0edce50c4c771ac9382
Syuparn <s.hello.spagetti@gmail.com>
2019-08-13 12:50:40 +0900

    applied to integer literal expression

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"../lexer"
 	"../token"
 	"fmt"
+	"strconv"
 )
 
 type Parser struct {
@@ -27,6 +28,7 @@ func New(l *lexer.Lexer) *Parser {
 	// pratt構文解析器の構文解析関数を初期化
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	p.nextToken()
 	p.nextToken()
@@ -154,6 +156,21 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+
+	return lit
 }
 
 const ( // operand priority
