ec06d2d5cd9972676f0f2c294b0ced3090e2b10c
Syuparn <s.hello.spagetti@gmail.com>
2019-08-17 11:50:30 +0900

    enable to eval integer literal

---
+++ evaluator/evaluator.go
@@ -0,0 +1,33 @@
+package evaluator
+
+import (
+	"../ast"
+	"../object"
+)
+
+// 全ての子ノードを再帰的にたどり評価
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, stmt := range stmts {
+		result = Eval(stmt)
+	}
+
+	return result
+}
