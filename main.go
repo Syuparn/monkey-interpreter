fd40e542ba6835ae4c90f7a8b9f988b8d6aa3001
Syuparn <s.hello.spagetti@gmail.com>
2019-08-26 18:13:16 +0900

    enable to read script file

--- main.go
+++ main.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"./repl"
+	"./runscript"
+	"flag"
 	"fmt"
 	"os"
 	"os/user"
 )
 
+var (
+	scriptFileName = flag.String("f", "",
+		"monkey script file name to run (run REPL instead if empty)")
+)
+
 func main() {
+	flag.Parse()
+
+	if *scriptFileName != "" {
+		runScriptFile(*scriptFileName)
+	} else {
+		runRepl()
+	}
+}
+
+func runRepl() {
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
@@ -16,4 +33,11 @@ func main() {
 		user.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
+}
+
+func runScriptFile(fileName string) {
+	_, err := runscript.RunScript(fileName)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
 }
---
+++ runscript/runscript.go
@@ -0,0 +1,82 @@
+package runscript
+
+import (
+	"../evaluator"
+	"../lexer"
+	"../object"
+	"../parser"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func RunScript(fileName string) (*object.Environment, error) {
+
+	scriptCode, err := readScript(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	l := lexer.New(scriptCode)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	if len(p.Errors()) != 0 {
+		return nil, formatParserErrors(p.Errors())
+	}
+
+	env := object.NewEnvironment()
+	evaluated := evaluator.Eval(program, env)
+
+	if errObj, ok := evaluated.(*object.Error); ok {
+		return nil, formatEvaluatorErrors(errObj)
+	}
+
+	return env, nil
+}
+
+func readScript(fileName string) (string, error) {
+	f, err := os.Open(fileName)
+	defer f.Close()
+	if err != nil {
+		return "", fmt.Errorf("file could not open: %s", fileName)
+	}
+
+	script, err := ioutil.ReadAll(f)
+	if err != nil {
+		return "", fmt.Errorf("file could not read: %s", fileName)
+	}
+
+	return string(script), nil
+}
+
+const MONKEY_FACE = `
+           __,__
+  .--.  .-"     "-.  .--.
+ / .. \/  .-. .-.  \/ .. \
+| |  '|  /   Y   \  |'  | |
+| \   \  \ 0 | 0 /  /   / |
+ \ '- ,\.-"""""""-./, -' /
+  ''-' /_   ^ ^   _\ '-''
+      |  \._   _./  |
+      \   \ '~' /   /
+       '._ '-=-' _.'
+          '-----'
+`
+
+func formatParserErrors(errors []string) error {
+	var errMsg bytes.Buffer
+	errMsg.WriteString(MONKEY_FACE)
+	errMsg.WriteString("Woops! We ran into some monkey business here!\n")
+	errMsg.WriteString(" parser errors:\n")
+	for _, err := range errors {
+		errMsg.WriteString("\t" + err + "\n")
+	}
+
+	return fmt.Errorf(errMsg.String())
+}
+
+func formatEvaluatorErrors(errObj *object.Error) error {
+	return fmt.Errorf(errObj.Message)
+}
