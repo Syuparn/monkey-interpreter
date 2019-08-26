5f563504035598aad39b2a7b69bf7343b45153c1
Syuparn <s.hello.spagetti@gmail.com>
2019-08-26 19:21:40 +0900

    change path of runscript and add evalscript to avoid circular reference

---
+++ evaluator/evalscriptfile_test.go
@@ -0,0 +1,46 @@
+package evaluator
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRunScriptErrors(t *testing.T) {
+	tests := []struct {
+		fileName string
+		expected string
+	}{
+		// reading error
+		{
+			"errsample/notexistingfile.monkey",
+			"file could not open: errsample/notexistingfile.monkey",
+		},
+		// parser error
+		{
+			"errsample/err_fail2parse.monkey",
+			(MONKEY_FACE +
+				"Woops! We ran into some monkey business here!\n" +
+				" parser errors:\n" +
+				"\tno prefix parse function for + found\n"),
+		},
+		// evaluator error
+		{
+			"errsample/err_no_ident.monkey",
+			"identifier not found: a",
+		},
+	}
+
+	for _, tt := range tests {
+		_, err := EvalScriptFile(tt.fileName)
+
+		if err == nil {
+			t.Fatalf("err should occur. fileName=%s", tt.fileName)
+			return
+		}
+
+		if fmt.Sprintf("%s", err) != tt.expected {
+			t.Fatalf("error message was wrong. got=\n%s\n, expected=\n%s\n",
+				fmt.Sprintf("%s", err), tt.expected)
+		}
+	}
+}
